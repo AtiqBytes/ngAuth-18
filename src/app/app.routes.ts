b2c78a1cdbdf9fe596898fda2e119cceff9fc919
atiqBytes <atiqbytes@gmail.com>
2024-09-15 12:17:35 +0500

    setup routes for the components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { HomeComponent } from './Components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'login',
+        component:LoginComponent,
+        pathMatch:'full'
+    },
+
+    {
+        path:'register',
+        component:RegisterComponent,
+        pathMatch:'full'
+    },
+
+    {
+        path:'',
+        component:HomeComponent,
+        pathMatch:'full'
+    },
+];
